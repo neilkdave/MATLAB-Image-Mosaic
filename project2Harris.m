d85f29fac8c3d4dbeb3151b355d5b3c8ec4d8e09
Neil Dave <neil_k_dave@yahoo.com>
2015-11-13 11:42:55 -0500

    Added Correspondences
    added code to calculate correspondences, numbers work out, still have to
    find MATLAB tools to visualize them.

--- project2Harris.m
+++ project2Harris.m
@@ -28,3 +28,35 @@ plot(C1(:,2),C1(:,1),'b*')
 figure, imshow(image2);
 hold on
 plot(C2(:,2),C2(:,1),'b*')
+
+% normalized cross correlations
+
+Correspondences = zeros(length(C1), 2, 2);
+count = 0;
+for i = 1:length(C1)
+    y1 = C1(i,1);
+    x1 = C1(i,2);
+    if x1 <= 10 || y1 <= 10 || x1 > 512-10 || y1 > 340-10
+        continue;
+    end
+    %Creates 20 by 20 Window around each corner in each image
+    w1 = gray1(y1-10:y1+10,x1-10:x1+10);
+    ccs = zeros(length(C2), 1);
+    for j = 1:length(C2)
+        y2 = C2(j,1);
+        x2 = C2(j,2);
+        if x2 <= 10 || y2 <= 10 || x2 > 512-10 || y2 > 340-10
+            continue;
+        end
+        w2 = gray2(y2-10:y2+10,x2-10:x2+10);
+        ncc = normxcorr2(w1, w2);
+        ccs(j,1) = ncc(21,21);
+    end
+    [cc, index] = max(ccs);
+    if cc > 0.9
+        count = count+1;
+        Correspondences(count,1:2,1) = [y1,x1];
+        Correspondences(count,1:2,2) = C2(index,1:2);
+    end
+end
+Correspondences = Correspondences(1:count,:,:);
